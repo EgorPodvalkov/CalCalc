7ba52e83dc340879ce1bf78aee0506ea7a2c037d
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-05-06 18:23:25 +0300

    Created ASP MVC project and updated .gitignore file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalCalc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
