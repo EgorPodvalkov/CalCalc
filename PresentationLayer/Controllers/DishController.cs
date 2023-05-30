04fb3d798f4d64c1ed187221c1e1fffbc90614a4
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-05-30 10:30:30 +0300

    Added some dishes to DishController

--- PresentationLayer/Controllers/DishController.cs
+++ PresentationLayer/Controllers/DishController.cs
@@ -3,7 +3,6 @@ using BusinessLogicLayer.Interfaces;
 using BusinessLogicLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using PresentationLayer.DTOs;
-using System;
 using System.Text.Json;
 
 namespace PresentationLayer.Controllers;
@@ -32,7 +31,17 @@ public class DishController : Controller
 
         if (dishes.Count == 0)
         {
-            await _dishService.AddDishesAsync("Cheesecake pasta French fries boiled potato salad steak cutlet burger mushroom risotto bread");
+            await _dishService.AddDishesAsync(
+                "Cheesecake pasta French fries boiled potato salad steak cutlet burger " +
+                "mushroom risotto bread Pizza Ramen Paella Moussaka Coleslaw Dashi ravioli " +
+                "jiaozi Sashimi Yakiniku Tortilla croissant Tacos  Sushi tonkatsu Yakitori" +
+                "gnocchi Ceviche Quesadilla Fajitas Guacamole hummus Tamal Burrito barbecue" +
+                "Mac and cheese Biryani Pho carbonara nachos brownies Cheeseburger paella " +
+                "wonton Quiche fried chicken gyoza kimchi cupcake doughnut Churrasco " +
+                "Mochi onigiri Roti tiramisu Fondue Neapolitan pizza Milkshake Grilled cheese " +
+                "Miso soup Dal apple pie sandwich Chicken Kyiv Olivier Potato Salad " +
+                "Sauerkraut Soup Potato Pancakes Crepes Buckwheat Porridge " +
+                "Garlic Pampushki jam ");
             dishes = _mapper.Map<ICollection<DishDTO>>(await _dishService.GetAllAsync());
         }
 
